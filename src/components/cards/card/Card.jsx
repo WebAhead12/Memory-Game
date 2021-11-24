fd926ff128edfebf9c68f6bcdc17d8b4e38833c2
deana17 <91081633+deana17@users.noreply.github.com>
2021-11-24 15:39:22 +0200

    covers are up!

--- src/components/cards/card/Card.jsx
+++ src/components/cards/card/Card.jsx
@@ -1,18 +1,14 @@
-import React from 'react';
-import './style.css';
+import React from "react";
+import "./style.css";
 // import image from '../images/0.jpg';
-const path = require('path')
 
-
-function Card(props){
-    let mypath=path.join(__dirname,'..','images')
-    console.log(mypath);
-    // const [imgUrl, setImgUrl] = React.useState('0.jpg')
-    return (
-        <div className='card'>
-            <img className='cardCover' src={mypath+'/0.jpg'} alt='cover'/>
-        </div>
-    );
+function Card(props) {
+  // const [imgUrl, setImgUrl] = React.useState('0.jpg')
+  return (
+    <div className="card">
+      <img className="cardCover" src={"/images/0.jpg"} alt="cover" />
+    </div>
+  );
 }
 
 export default Card;
